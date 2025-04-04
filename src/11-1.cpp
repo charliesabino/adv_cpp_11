e92dfa8d1b83ff349c7ddc22c0cbe9f2455844a0
Charlie Sabino <94320712+charliesabino@users.noreply.github.com>
2025-04-04 09:35:33 -0500

    Add static assert

--- src/11-1.cpp
+++ src/11-1.cpp
@@ -26,6 +26,8 @@ int main() {
   using T1 = std::tuple<int, int, double, float>;
   using T2 = ReplaceAll<T1, int, double>::type;
   T2 t{1.1, 2.2, 3, 4};
+  static_assert(std::is_same_v<T2, std::tuple<double, double, double, float>>);
+
   std::cout << std::format("Elements at indexes 0 and 1 are {} and {} -- "
                            "replaced ints with doubles!\n",
                            std::get<0>(t), std::get<1>(t));
